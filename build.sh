4db5b09356dc16a9651033f2d18ed1390d24c856
rkolesnikIamCore <137054668+rkolesnikIamCore@users.noreply.github.com>
2025-06-18 23:09:06 +0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+mkdir work1
+cd work1
+
+git clone https://github.com/Kaaiot/prometheus-community-openshift.git
+cd prometheus-community-openshift
+git config user.email $EMAIL
+git config user.name $USERNAME
+git remote set-url origin https://$USERNAME:$TOKEN@github.com/Kaaiot/prometheus-community-openshift.git
+cd ..
+
+#download helm
+wget https://get.helm.sh/helm-v3.18.3-linux-amd64.tar.gz
+tar xzf helm-v3.18.3-linux-amd64.tar.gz
+mv linux-amd64/helm helm
+#helm is now work1/helm
+rm -f helm-v3.18.3-linux-amd64.tar.gz
+rm -rf linux-amd64
+
+#download cr
+wget https://github.com/helm/chart-releaser/releases/download/v1.8.1/chart-releaser_1.8.1_linux_amd64.tar.gz
+tar xzf chart-releaser_1.8.1_linux_amd64.tar.gz
+#cr is now work1/cr
+rm -f chart-releaser_1.8.1_linux_amd64.tar.gz
+
+#download yq
+wget "https://github.com/mikefarah/yq/releases/download/v4.44.1/yq_linux_amd64" -O yq
+chmod +x yq
+#yq is now work1/yq
+
+#download gh-cli
+wget https://github.com/cli/cli/releases/download/v2.74.1/gh_2.74.1_linux_amd64.tar.gz
+tar xzf gh_2.74.1_linux_amd64.tar.gz
+#gh in /work1/gh_2.74.1_linux_amd64/bin
+echo $TOKEN | ./gh_2.74.1_linux_amd64/bin/gh auth login --with-token
+
+#adding deps
+for file in $(find prometheus-community-openshift/charts/ -name 'Chart.yaml'); do
+  ./yq '.dependencies[] | "./helm repo add \(.name) \(.repository)"' $file >> deps.sh
+done
+bash deps.sh
+rm -f deps.sh
+
+#building shit
+for chart in $(find prometheus-community-openshift/charts -maxdepth 1 -type d); do
+  ./cr package $chart
+done
+#all stuff is now work1/.cr-release-packages
+
+cd prometheus-community-openshift
+
+#killing releases and tags
+../gh_2.74.1_linux_amd64/bin/gh release list | sed 's/|/ /' | awk '{print $1, $8}' | while read -r line; do .././gh_2.74.1_linux_amd64/bin/gh release delete -y "$line" --cleanup-tag; done
+
+#deleting old files
+git checkout gh-pages
+find . -name '*.tgz' -type f -delete
+rm -f index.yaml
+git add -u
+git commit -m "Remove all old .tgz files"
+git push origin gh-pages
+git checkout main
+
+git remote set-url origin https://github.com/Kaaiot/prometheus-community-openshift.git
+#upload packages
+../cr upload -o $USERNAME -r prometheus-community-openshift --packages-with-index -p ../.cr-release-packages -t $TOKEN --push
+#create index
+../cr index -o $USERNAME -r prometheus-community-openshift --packages-with-index -i . -p ../.cr-release-packages -t $TOKEN --push
+
+cd ../../
+rm -rf work1
